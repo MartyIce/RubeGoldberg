1f15ef3e91995e7b6fc3c4f5f7e7d105ccae4f2b
LAPTOP-GASN6S89\Marty Mavis <marty.mavis@rev.com>
2022-11-18 22:28:42 -0600

    simple navigation and tailwind

---
+++ react-admin-ui/src/DynamoDB.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h2>Play with DynamoDB</h2>
+      </div>
+    );
+  }
+  
---
+++ react-admin-ui/src/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h2>Nothing to see here</h2>
+        <img src="/rg-logo.png" alt="logo"/>
+      </div>
+    );
+  }
+  
